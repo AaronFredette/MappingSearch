c2d095a8f6111cc337bf330b1c99b5c011969f0d
Aaron Fredette <Aaron.Fredette@velir.com>
2013-06-28 11:01:57 -0400

    added review area

---
+++ MappingSearch/Controllers/Reviews/ProductReviewController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MappingSearch.Models.Reviews;
+
+namespace MappingSearch.Controllers.Reviews
+{
+    public class ProductReviewController : Controller
+    {
+        //
+        // GET: /ProductReview/
+
+        public ActionResult Index()
+        {
+
+            return View(new ProductCategoryBrandModel());
+        }
+
+    }
+}
---
+++ MappingSearch/Models/Reviews/ProductCategoryBrandModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MappingSearch.Models.Reviews
+{
+    public class ProductCategoryBrandModel
+    {
+        public Dictionary<string,string> Categories { get; set; }
+        public Dictionary<string,string> Brands { get; set; }
+    }
+}
